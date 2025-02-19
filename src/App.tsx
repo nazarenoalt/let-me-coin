05e0c9b644219d62045feaed48d67b656cd50ce4
Nazareno Altamiranda <nazareno.anzar@gmail.com>
2025-02-19 20:22:37 -0300

    a lot fo changes (I swear this is the last time I push a bulk of unrelated things)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import GlobalStyles from './globalStyles.js';
-
+import HomePage from './pages/HomePage/HomePage';
+import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
+import { Error404 } from './pages/Error404/Error404';
+import { MobileLayout } from './layouts/MobileLayout/MobileLayout.js';
 function App() {
 
   return (
     <>
-      <h1>Let Me Coin</h1>
-      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route element={<MobileLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="*" element={<Error404 />} />
+          </Route>
+        </Routes>
+      </Router>
+    <GlobalStyles />
     </>
   )
 }
+
 
 export default App
--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,11 +1,42 @@
 import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
-  body {
+
+  :root {
+    /* Base Colors */
+    --white:rgb(240, 240, 240);
+    --black:rgb(23, 23, 23);
+    
+    /* Brillant colors */
+    --neon-green: #05e316;
+    --neon-blue:rgb(30, 109, 255);
+    --neon-red: #ff3838;
+    --neon-yellow: #f7d51d;
+    --neon-violet: #9b5de5;
+    --neon-orange: #ff9f1a;
+    
+    /* Dark colors */
+    --neon-dark-green: #218c5e;
+    --neon-dark-blue: #0e7fae;
+    --neon-dark-red: #b22222;
+    --neon-dark-yellow: #b89b16;
+    --neon-dark-violet: #6a2c90;
+    --neon-dark-orange: #b4640a;
+    --dark-gray:rgb(12, 12, 12);
+
+    /* Background */
+    --background-color: var(--dark-gray);
+    --text-color: var(--white);
+  }
+  * {
+    box-sizing: border-box;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
     margin: 0;
     padding: 0;
-    background: teal;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    color: var(--text-color);
+  }
+  body {
+    background-color: var(--background-color);
   }
 `;
  
